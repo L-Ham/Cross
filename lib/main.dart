ec4153ab2342dd7a05ef056f401ef82bf9fd7ee3
YoussefZaki119 <103706498+YoussefZaki119@users.noreply.github.com>
2024-03-16 21:55:19 +0200

    mm

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
+import 'create_community.dart';
+import 'blocked_accounts.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(Myapp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: BlockedAccount(),
     );
   }
 }
