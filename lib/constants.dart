458621080d6ce140904ca867235eb05a3e4fb695
Peter Ashraf <Peter.ashraff26@gmail.com>
2024-03-17 22:50:53 +0200

    Bug Fixes -> Create Community rotation Issue fixes

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 const kPageTitleFontSizeHeightRatio = 0.03;
+const kPageTtleTopBottomPadding =  20.0;
+const kErrorTextTopBottomPadding = 10.0;
+const kPageEdgesPadding = 9.0;
 const kPageSubtitleFontSizeHeightRatio = 0.02;
 const kCommunityNameMinLength = 3;
 const kCommunityNameMaxLength = 21;
@@ -8,6 +11,9 @@ const kCommunityTypeDescriptionHeightRatio = 0.018;
 const kCreateCommunityButtonTextHeightRatio = 0.033;
 const kCreateCommunityButtonHeightRatio = 0.075;
 const kErrorTextHeightRatio = 0.018;
+const kCommunityTypeShowModalMaxHeightRatio = 0.35;
+const kCommunityTypeArrowDownIconSize = 0.045;
+const kErrorDisplayDelayTimeMilliseconds = 300;
 
 const kNotificationSettingsPrivateMessagesSwitchIndex = 0;
 const kNotificationSettingsChatMessagesSwitchIndex = 1;
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:reddit_bel_ham/screens/create_community_screen.dart';
 import 'screens/account_settings_screen.dart';
+import 'utilities/screen_size_handler.dart';
 import 'screens/notifications_screen.dart';
 import 'screens/first_screen.dart';
 
 
 void main() {
-  runApp(const RedditBElham());
+  runApp(const RedditByLham());
 }
 
-class RedditBElham extends StatelessWidget {
-  const RedditBElham({
+class RedditByLham extends StatelessWidget {
+  const RedditByLham({
     super.key,
   });
-
   @override
   Widget build(BuildContext context) {
+      ScreenSizeHandler.initialize(
+      MediaQuery.of(context).size.width, MediaQuery.of(context).size.height);
+      
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
       routes: {
+        "first_screen": (context) => FirstScreen(),
         "create_community_screen": (context) => const CreateCommunityScreen(),
         "account_settings_screen": (context) => const AccountSettingsScreen(),
-        "notification_settings_screen": (context) => NotificationSettingsScreen(),
-        "first_screen": (context) => FirstScreen(),
+        "notification_settings_screen": (context) => const NotificationSettingsScreen(),
 
       },
-      initialRoute: "first_screen",
+      initialRoute: "notification_settings_screen",
     );
   }
 }
