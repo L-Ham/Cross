3f2ca0346ce277fd62febc4dceff6a930289336c
Peter Ashraf <Peter.ashraff26@gmail.com>
2024-03-25 20:18:33 +0200

    Integerated the side bar with home screen

--- lib/screens/home_page_screen.dart
+++ lib/screens/home_page_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:reddit_bel_ham/components/home_page_components/post_card.dart';
+import 'package:reddit_bel_ham/components/settings_components/settings_tile.dart';
+import 'package:reddit_bel_ham/components/settings_components/settings_tile_leading_icon.dart';
 import 'package:reddit_bel_ham/constants.dart';
 import 'package:reddit_bel_ham/screens/home_page_seach_screen.dart';
+import 'package:reddit_bel_ham/utilities/screen_size_handler.dart';
 
 class HomePageScreen extends StatefulWidget {
 
@@ -12,6 +15,14 @@ class HomePageScreen extends StatefulWidget {
 }
 
 class _HomePageScreenState extends State<HomePageScreen> {
+
+    final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  String username = "peter_ashraf";
+  String onlineStatusString = "On";
+  bool onlineStatusToggle = true;
+  Color onlineStatusColor = kOnlineStatusColor;
+  double onlineStatusWidth = ScreenSizeHandler.smaller * 0.42;
+
   String selectedMenuItem = "Home";
   final List<String> menuItems = ['Home', 'Popular', 'Latest News'];
   final List<Post> posts = [
@@ -36,13 +47,19 @@ class _HomePageScreenState extends State<HomePageScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         leading: Padding(
           padding: const EdgeInsets.only(left: 8.0, right: 2.0),
-          child: Icon(
-            Icons.menu,
-            size: 30,
-            color: Colors.white,
+          child: GestureDetector(
+            onTap: () {
+              _scaffoldKey.currentState?.openDrawer();
+            },
+            child: Icon(
+              Icons.menu,
+              size: 30,
+              color: Colors.white,
+            ),
           ),
         ),
         title: Row(
@@ -81,8 +98,13 @@ class _HomePageScreenState extends State<HomePageScreen> {
           ),
           Padding(
             padding: const EdgeInsets.all(10.0),
-            child: CircleAvatar(
-              radius: 25,
+            child: GestureDetector(
+              onTap: () {
+                _scaffoldKey.currentState?.openEndDrawer();
+              },
+              child: CircleAvatar(
+                radius: 25,
+              ),
             ),
           ),
         ],
@@ -92,6 +114,145 @@ class _HomePageScreenState extends State<HomePageScreen> {
         itemBuilder: (context, index) {
           return buildPostCard(posts[index]);
         },
+      ),
+      drawer: Drawer(),
+      endDrawer: Drawer(
+        backgroundColor: kBackgroundColor,
+        child: SafeArea(
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(
+                    Icons.close,
+                    color: Colors.white,
+                    size: ScreenSizeHandler.bigger*kSideBarCloseIconSizeRatio,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    bottom: ScreenSizeHandler.screenHeight * 0.02),
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Text('A'),
+                  radius: ScreenSizeHandler.bigger * kSideBarCircleAvatarRadiusRatio,
+                  foregroundImage: AssetImage('assets/images/reddit_logo.png'),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    bottom: ScreenSizeHandler.screenHeight * 0.015),
+                child: Text('u/$username',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: ScreenSizeHandler.bigger * 0.026,
+                        fontWeight: FontWeight.bold)),
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    onlineStatusToggle = !onlineStatusToggle;
+                    if (onlineStatusToggle) {
+                      onlineStatusString = "On";
+                      onlineStatusColor = Color.fromARGB(255, 0, 204, 120);
+                      onlineStatusWidth = ScreenSizeHandler.smaller * 0.42;
+                    } else {
+                      onlineStatusString = "Off";
+                      onlineStatusColor = Colors.grey;
+                      onlineStatusWidth = ScreenSizeHandler.screenWidth * 0.38;
+                    }
+                  });
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 15.0),
+                  child: Container(
+                    height: ScreenSizeHandler.bigger * 0.04,
+                    width: onlineStatusWidth,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20), // adjust the radius as needed
+                      border: Border.all(
+                        color: onlineStatusColor, // set border color
+                        width: ScreenSizeHandler.smaller*0.006, // set border width
+                      ),
+                    ),
+                    child: Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          if (onlineStatusToggle)
+                            Icon(
+                              Icons.circle,
+                              color: onlineStatusColor,
+                              size: kOnlineStatusIconSize,
+                            ),
+                          Text(
+                            'Online Status: $onlineStatusString',
+                            style: TextStyle(
+                                color: onlineStatusColor,
+                                fontSize: ScreenSizeHandler.smaller * kOnlineStatusFontSizeRatio,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ListView(
+                  scrollDirection: Axis.vertical,
+                  children: [
+                    SettingsTile(
+                      leadingIcon: const SettingsTileLeadingIcon(
+                        leadingIcon: Icons.account_circle_outlined,
+                      ),
+                      titleText: "Profile",
+                      onTap: () {},
+                    ),
+                    SettingsTile(
+                      leadingIcon: const SettingsTileLeadingIcon(
+                        leadingIcon: Icons.groups_rounded,
+                      ),
+                      titleText: "Create a community",
+                      onTap: () {
+                        Navigator.pushNamed(context, 'create_community_screen');
+                      },
+                    ),
+                    SettingsTile(
+                      leadingIcon: const SettingsTileLeadingIcon(
+                        leadingIcon: Icons.bookmarks_outlined,
+                      ),
+                      titleText: "Saved",
+                      onTap: () {},
+                    ),
+                    SettingsTile(
+                      leadingIcon: const SettingsTileLeadingIcon(
+                        leadingIcon: Icons.access_time_rounded,
+                      ),
+                      titleText: "History",
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ),
+              SettingsTile(
+                leadingIcon: const SettingsTileLeadingIcon(
+                  leadingIcon: Icons.settings_outlined,
+                ),
+                titleText: "Settings",
+                trailingWidget: Icon(Icons.nights_stay_sharp, size: 25),
+                onTap: () {
+                  Navigator.pushNamed(context, 'account_settings_screen');
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
