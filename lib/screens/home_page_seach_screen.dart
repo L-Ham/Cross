8a25938042857f0367c079a6d3214883fc56045b
faridaSalem <faridayasser45@gmail.com>
2024-03-26 16:24:22 +0200

    fixed padding of search bar

--- lib/screens/home_page_seach_screen.dart
+++ lib/screens/home_page_seach_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:reddit_bel_ham/constants.dart';
+import 'package:reddit_bel_ham/utilities/screen_size_handler.dart';
 
 class SearchScreen extends StatefulWidget {
   @override
@@ -37,7 +38,7 @@ class _SearchScreenState extends State<SearchScreen> {
               Navigator.pop(context);
             },
             child: Padding(
-              padding: const EdgeInsets.only(right: 8.0),
+              padding: EdgeInsets.only(right:ScreenSizeHandler.screenWidth * 0.05),
               child: Text('Cancel',
                   style: TextStyle(color: Color.fromARGB(255, 81, 81, 81))),
             ),
