53bd4cf5fc8683bcda966e86952d0f6c2e799cee
Peter Ashraf <Peter.ashraff26@gmail.com>
2024-05-06 23:58:03 +0300

    Android notifications implemented

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,17 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  try {
+    await Firebase.initializeApp();
+    print('Handling a background message ${message.messageId}');
+  } catch (e) {
+    print(e);
+  }
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -46,6 +57,57 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  final AndroidInitializationSettings initializationSettingsAndroid =
+      const AndroidInitializationSettings('elham_final_logo');
+  final InitializationSettings initializationSettings =
+      InitializationSettings(android: initializationSettingsAndroid);
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  FirebaseMessaging.instance.getToken().then((String? token) {
+    if (token != null) {
+      print('FCM Token: $token');
+    }
+  });
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    RemoteNotification? notification = message.notification;
+    AndroidNotification? android = message.notification?.android;
+
+    if (android != null) {
+      flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification?.title,
+          notification?.body,
+          const NotificationDetails(
+            android: AndroidNotificationDetails(
+              'reddit_bel_ham',
+              'reddit_bel_ham channel',
+              icon: 'elham_final_logo',
+              playSound: true,
+              enableVibration: true,
+              priority: Priority.high,
+              importance: Importance.max,
+            ),
+          ));
+    }
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+    print('A new onMessageOpenedApp event was published!');
+  });
+
   String? token = prefs.getString('token');
   if (token != null) {
     TokenDecoder.updateToken(token);
